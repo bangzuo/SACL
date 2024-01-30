d994bfd7e102f0dd384591e4902846981d0833d9
bangzuo <zhangbz@nenu.edu.cn>
2024-01-30 20:51:41 +0800

    Add files via upload

--- utils/parser.py
+++ utils/parser.py
@@ -2,16 +2,8 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="MKG")
-    
-    # ===== 消融实验 ===== #
-    parser.add_argument('--no_kgAug', type=bool, default=False, help='no_kgAug')
-    parser.add_argument('--no_uiAug', type=bool, default=False, help='no_uiAug')
-    parser.add_argument('--no_cross_cl', type=bool, default=False, help='no_cross_cl')
-    parser.add_argument('--no_ad', type=bool, default=False, help='no_ad')
-    parser.add_argument('--no_viewGen', type=bool, default=False, help='no_viewGen')
-    parser.add_argument('--no_denoise', type=bool, default=False, help='no_denoise')
-    
+    parser = argparse.ArgumentParser(description="SACL")
+
     # ===== log ===== #
     parser.add_argument('--desc', type=str, default="", help='EXP description')
     parser.add_argument('--log', action='store_true', default=False, help='log in file or not')
@@ -24,14 +16,6 @@ def parse_args():
     parser.add_argument('--mae', action='store_true', default=False, help='use MAE or not')
     parser.add_argument('--ab', type=str, default=None, help='ablation study')
     # ===== Model HPs ===== #
-
-    # 控制知识图谱的超参数
-    parser.add_argument('--mae_rate', type=int, default=0.1, help='mask size for MAE')
-    parser.add_argument('--cross_cl_reg', type=float, default=0.01, help='coefficient for CL loss')
-    parser.add_argument('--cross_cl_tau', type=float, default=1.0, help='temperature for CL')
-
-    parser.add_argument('--kg_dropout', type=bool, default=True, help='kg dropout')
-    parser.add_argument('--ui_dropout', type=bool, default=True, help='ui dropout')
 
     # ===== train ===== #
     parser.add_argument('--epoch', type=int, default=1000, help='number of epochs')
@@ -51,14 +35,19 @@ def parse_args():
     parser.add_argument("--batch_test_flag", type=bool, default=True, help="use gpu or not")
     parser.add_argument("--cuda", type=int, default=1, help="use gpu or not")
     parser.add_argument("--gpu_id", type=int, default=0, help="gpu id")
-
-    # 知识图谱端对比学习
-    parser.add_argument('--kg_cl_tau', default=0.3, type=float, help='温度系数for知识对比学习')
-    parser.add_argument('--kg_cl_reg', default=0.001, type=float, help='损失权重for知识对比学习')
+    parser.add_argument('--cross_cl_reg', type=float, default=0.01, help='contrastive learning loss weights')
+    parser.add_argument('--cross_cl_tau', type=float, default=1.0, help='temperature hyperparameters for contrastive learning')
 
     parser.add_argument('--Ks', nargs='?', default='[20]', help='Output sizes of every layer')
     parser.add_argument('--test_flag', nargs='?', default='part',
                         help='Specify the test type from {part, full}, indicating whether the reference is done in mini-batch')
+
+    parser.add_argument('--mae_rate', type=int, default=0.1, help='mask size for MAE')
+    parser.add_argument('--kg_dropout', type=bool, default=True, help='kg dropout')
+    parser.add_argument('--ui_dropout', type=bool, default=True, help='ui dropout')
+
+    parser.add_argument('--aug_kg_rate', type=float, default=0.2, help='long-tail augmentation keeping rate for knowledge graph')
+    parser.add_argument('--aug_ui_rate', type=float, default=0.2, help='long-tail augmentation keeping rate for user-item graph')
 
     parser.add_argument("--annealing_type", type=int, default=0, help="annealing_type：0、1、2...")
 
