56386f4050cc3966c853db119ea61d581969f7b9
chuxin01 <88866339+chuxin01@users.noreply.github.com>
2024-02-08 15:04:31 +0800

    Update parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -31,18 +31,18 @@ def parse_args():
     parser.add_argument("--cuda", type=int, default=1, help="use gpu or not")
     parser.add_argument("--gpu_id", type=int, default=0, help="gpu id")
     parser.add_argument('--cross_cl_reg', type=float, default=0.01, help='contrastive learning loss weights')
-    parser.add_argument('--cross_cl_tau', type=float, default=1.0, help='temperature hyperparameters for contrastive learning')
+    parser.add_argument('--cross_cl_tau', type=float, default=0.1, help='temperature hyperparameters for contrastive learning')
 
     parser.add_argument('--Ks', nargs='?', default='[20]', help='Output sizes of every layer')
     parser.add_argument('--test_flag', nargs='?', default='part',
                         help='Specify the test type from {part, full}, indicating whether the reference is done in mini-batch')
 
-    parser.add_argument('--mae_rate', type=float, default=0.1, help='mask size for MAE')
+    parser.add_argument('--mae_rate', type=float, default=0.05, help='mask size for MAE')
     parser.add_argument('--kg_dropout', type=bool, default=True, help='kg dropout')
     parser.add_argument('--ui_dropout', type=bool, default=True, help='ui dropout')
 
     parser.add_argument('--aug_kg_rate', type=float, default=0.2, help='long-tail augmentation keeping rate for knowledge graph')
-    parser.add_argument('--aug_ui_rate', type=float, default=0.2, help='long-tail augmentation keeping rate for user-item graph')
+    parser.add_argument('--aug_ui_rate', type=float, default=0.8, help='long-tail augmentation keeping rate for user-item graph')
     parser.add_argument('--kl_eps', type=float, default=0.1, help='KL weight')
     parser.add_argument('--mi_eps', type=float, default=0.1, help='Missing weight')
 
